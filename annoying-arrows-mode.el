79f4d4f482ebbf7b3f5c417daa9b5c7cedbac68c
Magnar Sveen <magnars@gmail.com>
2012-04-12 07:05:41 +0200

    Initial commit.

---
+++ annoying-arrows-mode.el
@@ -0,0 +1,68 @@
+;;; annoying-arrows-mode.el --- Ring the bell if using arrows too much
+
+;; Copyright (C) 2011 Magnar Sveen
+
+;; Author: Magnar Sveen <magnars@gmail.com>
+;; Keywords: marking region
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Entering annoying-arrows-mode makes emacs ring the bell in your
+;; face if you use the arrow keys to move long distances.
+
+;; Set the `annoying-arrows-too-far-count' to adjust the length.
+
+;; Code:
+
+(defvar annoying-arrows-too-far-count 10
+  "Number of repeated arrow presses before emacs gets annoyed.")
+
+(setq annoying-commands '(previous-line
+                          next-line
+                          right-char
+                          left-char
+                          forward-char
+                          backward-char))
+
+(defvar annoying-arrows--current-count 0)
+
+(defmacro add-annoying-arrows-advice (cmd)
+  `(defadvice ,cmd (before annoying-arrows activate)
+     (when annoying-arrows-mode
+       (if (memq last-command annoying-commands)
+           (progn
+             (incf annoying-arrows--current-count)
+             (when (> annoying-arrows--current-count annoying-arrows-too-far-count)
+               (beep 1)
+               (message "Your incessant use of the arrow keys annoy me to no end.")))
+         (setq annoying-arrows--current-count 0)))))
+
+(add-annoying-arrows-advice previous-line)
+(add-annoying-arrows-advice next-line)
+(add-annoying-arrows-advice right-char)
+(add-annoying-arrows-advice left-char)
+(add-annoying-arrows-advice forward-char)
+(add-annoying-arrows-advice backward-char)
+
+(define-minor-mode annoying-arrows-mode
+  "Annoying-Arrows emacs minor mode."
+  nil "" nil)
+
+(define-globalized-minor-mode global-annoying-arrows-mode
+  annoying-arrows-mode annoying-arrows-mode)
+
+(provide 'annoying-arrows-mode)
+;; annoying-arrows-mode.el ends here
